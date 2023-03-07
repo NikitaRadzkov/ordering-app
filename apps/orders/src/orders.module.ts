752416ac9535df91aba273111335dd024cce70a9
Nikita Radzkou <nikita.radzkou@gmail.com>
2023-03-07 22:14:48 +0300

    Add repository and schema for mongo db

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,26 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from './orders.repository';
 import { OrdersService } from './orders.service';
+import { Order, OrderSchema } from './schemas/order.shema';
 
 @Module({
-  imports: [],
-  controllers: [OrdersController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
+  controllers: [OrdersController, OrdersRepository],
   providers: [OrdersService],
 })
 export class OrdersModule {}
